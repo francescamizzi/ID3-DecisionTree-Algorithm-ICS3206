9004ed4faea20074363a0359195bee908ab61551
francescamizzi <72987846+francescamizzi@users.noreply.github.com>
2022-01-21 08:44:13 +0100

    restart baby

--- id3.py
+++ id3.py
@@ -1,5 +1,14 @@
 import pandas as pd
+import numpy as np
 
-irisdata = pd.read_csv('iris.data', sep=",")
-winedata = pd.read_csv('wine.data', sep=",")
+irisdata = pd.read_csv('iris.data', sep=",", names=['sepalLength', 'sepalWidth', 'petalLength', 'petalWidth', 'class'])
+winedata = pd.read_csv('wine.data', sep=",", names=['Alcohol','Malic acid','Ash','Alcalinity of ash','Magnesium','Total phenols','Flavanoids','Nonflavanoid phenols','Proanthocyanins','Color intensity','Hue','OD280/OD315 of diluted wines','Proline'  ])
+
+def continuous_to_discrete():
+    for column in irisdata:
+        columnSeriesObj = irisdata[column]
+        temp = pd.cut(columnSeriesObj.values, bins=5)
+        irisdata.insert(1, column = columnSeriesObj.name, value = temp)
+    
+continuous_to_discrete()
 print(irisdata)
